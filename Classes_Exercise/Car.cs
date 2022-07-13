860a685479fe40985a8bfc8c5af7060a186c9e32
coreyareid <coreyareid@bellsouth.net>
2022-07-13 14:01:08 -0500

    Completed exercise

---
+++ Classes_Exercise/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Classes_Exercise;
+
+// Exercise 1
+public class Car
+{
+    // Peramerterized Constructor
+    public Car(string make, string model, int year, string color)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        Color = color;
+    }
+
+    //properties
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+
+    // Methods
+     public void TheCar()
+    {
+
+        Console.WriteLine("\n\n Vehicle Information");
+        Console.WriteLine($"Make of the car: {Make}");
+        Console.WriteLine($"Model of the car: {Model}");
+        Console.WriteLine($"Manufacture date: {Year}");
+        Console.WriteLine($"Car color: {Color}");
+    }
+
+
+
+}
+
---
+++ Classes_Exercise/Program.cs
@@ -0,0 +1,9 @@
+﻿// Exercise 1
+
+using Classes_Exercise;
+
+Car carOne = new Car("Honda", "Accord", 1997, "Black");
+Car carTwo = new Car("Porsche", "911", 2006, "White");
+
+carOne.TheCar();
+carTwo.TheCar();
